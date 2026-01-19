48124d2e44687ed01f7996c4a9a01fee6891db4e
Manuel Schneider <manuelschneid3r@gmail.com>
2026-01-19 15:38:15 +0100

    chore(release): v2.1.2

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from pathlib import Path
 from albert import *
 
 md_iid = "5.0"
-md_version = "2.1.1"
+md_version = "2.1.2"
 md_name = "Pomodoro"
 md_description = "Set up a Pomodoro timer"
 md_license = "MIT"
